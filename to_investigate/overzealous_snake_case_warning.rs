8498b02938d8b2e809ffbf5b1f2fcfb37d9ca799
Carol Nichols <carol.nichols@gmail.com>
2015-01-11 20:39:42 -0500

    Adding another note for the non_snake_case warning

--- to_investigate/overzealous_snake_case_warning.rs
+++ to_investigate/overzealous_snake_case_warning.rs
@@ -1,6 +1,7 @@
 // warning: module `ʷE` should have a snake case name such as `ʷ_e`, #[warn(non_snake_case)] on by default
 // warning: module `U` should have a snake case name such as `u`, #[warn(non_snake_case)] on by default
 // warning: module `Ê̷ç̫Úʿᾈ` should have a snake case name such as `ê̷ç̫_úʿᾈ`, #[warn(non_snake_case)] on by default
+// warning: module `ʷʺ̵‿̘` should have a snake case name such as `ʷʺ̵‿̘`, #[warn(non_snake_case)] on by default (investigate upper/lower casedness of this one, they look the same to my eye)
 #![feature(non_ascii_idents)]
   /* ᛭ĉþɍɅ */ //
  mod kʱ {  mod ʷE {  }  }  mod U {  mod Ê̷ç̫Úʿᾈ {  }  }
